a4df16768fdf376871873d06148e962582044abc
manucarvalho <manuela_carvalho_6@hotmail.com>
2021-06-25 18:40:18 -0300

    Exceção delete e update

--- src/main/java/com/estudo/course/services/UserService.java
+++ src/main/java/com/estudo/course/services/UserService.java
@@ -3,11 +3,16 @@ package com.estudo.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.estudo.course.entities.User;
 import com.estudo.course.repositories.UserRepository;
+import com.estudo.course.resources.exceptions.DatabaseException;
 import com.estudo.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,14 +35,25 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	@SuppressWarnings("deprecation")
 	public User update(Long id, User obj) {
-		User entity = repository.getOne(id);
-		updateData(entity,obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getOne(id);
+			updateData(entity,obj);
+			return repository.save(entity);
+		}catch(EntityNotFoundException e) {
+			 throw new ResourceNotFoundException(id);
+		}
+		
 	}
 
 	private void updateData(User entity, User obj) {
